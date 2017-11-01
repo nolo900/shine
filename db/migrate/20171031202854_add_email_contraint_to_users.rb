dd6ebcc1aba7d42d02d6e140d39140656f85a93a
Austin Miles <abm@austinbmiles.com>
2017-10-31 20:51:43 -0400

    added webpack and devise

---
+++ db/migrate/20171031202854_add_email_contraint_to_users.rb
@@ -0,0 +1,21 @@
+class AddEmailContraintToUsers < ActiveRecord::Migration[5.1]
+  def up
+    execute %{
+      ALTER TABLE
+        users
+      ADD CONSTRAINT
+        email_must_be_company_email
+      CHECK ( email ~* '^[^@]+@test\\.com$' )
+    }
+  end
+
+  def down
+    execute %{
+      ALTER TABLE
+        users
+      DROP CONSTRAINT
+        email_must_be_company_email
+    }
+  end
+
+end

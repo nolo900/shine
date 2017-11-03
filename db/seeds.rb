ed5a01d547d3221b2f60d4c86b35bbd9fa111592
Austin Miles <abm@austinbmiles.com>
2017-11-03 15:27:06 -0400

    modifications to seeds, reverted after heroku pg fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-350_000.times do |i|
+9_000.times do |i|
   Customer.create!(
               first_name: Faker::Name.first_name,
               last_name: Faker::Name.last_name,

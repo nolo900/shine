52e02020ddadbfc25a0485edb10277cd172e6fef
Austin Miles <abm@austinbmiles.com>
2018-01-08 11:42:20 -0500

    removed some angular file and dependencies, updated ruby version to 2.4.2

--- app/javascript/packs/shine_customer_search.js
+++
@@ -1,1 +0,0 @@
-require('../shine_customer_search');
--- app/javascript/shine_customer_search/app/customer.ts
+++
@@ -1,7 +0,0 @@
-export class Customer {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    username: string;
-}
